17f5d6091b2dac308d13a07d8dfc5ec2534582a6
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-19 08:21:01 +0300

    Update directory_path.dart

--- lib/directory_path.dart
+++ lib/directory_path.dart
@@ -5,7 +5,7 @@ import 'package:path_provider/path_provider.dart';
 class DirectoryPath {
   getPath() async {
     final Directory? tempDir = await getExternalStorageDirectory();
-    final filePath = Directory('${tempDir!.path}/files');
+    final filePath = Directory('${tempDir!.path}/media');
     if (await filePath.exists()) {
       return filePath.path;
     } else {
