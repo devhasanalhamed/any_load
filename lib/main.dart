cd4200752bc76d1ffaed4257f71408e5f3ff7d20
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-18 07:33:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:any_load/project/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('history');
   runApp(const MyApp());
 }
 
